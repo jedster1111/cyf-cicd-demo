8d9460300b6fea88614117e296d5ec416a54e88d
jedster1111 <jedster1111@hotmail.co.uk>
2024-06-07 22:52:48 +0100

    Add prettier

--- src/math.test.ts
+++ src/math.test.ts
@@ -1,6 +1,6 @@
-import { expect, test } from 'vitest'
-import { sum } from './math'
+import { expect, test } from "vitest";
+import { sum } from "./math";
 
-test('adds 1 + 2 to equal 3', () => {
-  expect(sum(1, 2)).toBe(3)
-})
+test("adds 1 + 2 to equal 3", () => {
+  expect(sum(1, 2)).toBe(3);
+});
